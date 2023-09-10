6c6972a4d0442b46e9b19a1de13f8f03837526aa
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-10 16:36:25 +0545

    feat: add a secondary color

--- lib/style/app_color.dart
+++ lib/style/app_color.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const Color primary = Color(0xFF128c7e);
+  static const Color secondary = Color.fromARGB(255, 185, 243, 187);
 }
