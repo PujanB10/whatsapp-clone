c4e48b067ad7ed356143b86c2099da337122626a
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-13 10:46:54 +0545

    fix: convert chatpage into stateless

--- lib/screens/chat_page/chat_page.dart
+++ lib/screens/chat_page/chat_page.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:whatsapp/screens/chat_page/widgets/chat_box.dart';
+import 'package:whatsapp/screens/chat_page/widgets/message_view_widget.dart';
 import 'package:whatsapp/screens/chat_page/view_model/chat_view_model.dart';
 import 'package:whatsapp/utils/styles/app_font_sizes.dart';
 import 'package:whatsapp/screens/chat_page/widgets/app_bar_widget.dart';
 
 /// Private chat page of the app.
-class ChatPage extends StatefulWidget {
+class ChatPage extends StatelessWidget {
   final String usrName;
   final String imgUrl;
   final String message;
 
   /// Private chat page that opens after having clicked
   /// a particular user from the home page.
-  const ChatPage(this.usrName, this.imgUrl, this.message, {super.key});
-  @override
-  State<StatefulWidget> createState() => ChatPageBody();
-}
-
-class ChatPageBody extends State<ChatPage> {
+  ChatPage(this.usrName, this.imgUrl, this.message, {super.key});
   final textController = TextEditingController();
   late String textMessage;
-  int numberOfMessages = 0;
-  bool isUser = false;
   var dynamicBottomBarIcon = const Icon(Icons.mic);
 
   @override
@@ -31,16 +24,14 @@ class ChatPageBody extends State<ChatPage> {
       resizeToAvoidBottomInset: true,
       backgroundColor: Colors.blueGrey.shade100,
 
-      /// Calls the custom AppBar widget.
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(55),
-          child: AppBarWidget(
-            username: widget.usrName,
-            imageUrl: widget.imgUrl,
-          )),
+      /// Calls the custom AppBar
+      appBar: AppBarWidget(
+        username: usrName,
+        imageUrl: imgUrl,
+      ),
 
       /// Calls the chat message builder.
-      body: MessagesViewWidget(widget: widget),
+      body: MessagesViewWidget(usrName: usrName),
       bottomNavigationBar: Padding(
         padding: MediaQuery.of(context).viewInsets,
         child: SizedBox(
@@ -88,8 +79,9 @@ class ChatPageBody extends State<ChatPage> {
                 padding: const EdgeInsets.fromLTRB(1, 0, 4, 5),
                 child: FloatingActionButton(
                   onPressed: () {
-                    context.read<ChatViewModel>().addMessages(
-                        widget.usrName, textMessage, isUser = true);
+                    context
+                        .read<ChatViewModel>()
+                        .addMessages(usrName, textMessage, true);
                     textController.clear();
                   },
                   child: context.watch<ChatViewModel>().defaultIcon,
@@ -112,36 +104,4 @@ Widget buildIconsInMessageField(Icon iconName) {
 }
 
 /// Builds the list of chat messages that comes in from the model.
-class MessagesViewWidget extends StatelessWidget {
-  const MessagesViewWidget({
-    super.key,
-    required this.widget,
-  });
 
-  final ChatPage widget;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        addAutomaticKeepAlives: false,
-        itemCount:
-            context.watch<ChatViewModel>().dummyChat[widget.usrName].length,
-        itemBuilder: ((BuildContext context, int index) => Column(
-              //aligning left if received message and right if sent message
-              crossAxisAlignment: (context
-                      .watch<ChatViewModel>()
-                      .dummyChat[widget.usrName][index]!["isUser"])
-                  ? CrossAxisAlignment.end
-                  : CrossAxisAlignment.start,
-              children: [
-                ChatBox(
-                    message: context
-                        .watch<ChatViewModel>()
-                        .dummyChat[widget.usrName]![index]["message"],
-                    isUser: context
-                        .watch<ChatViewModel>()
-                        .dummyChat[widget.usrName]![index]["isUser"])
-              ],
-            )));
-  }
-}
