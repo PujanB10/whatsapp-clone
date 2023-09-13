c5f0152bace3d37e69e07b0746adee6ae5bd4043
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-13 13:03:38 +0545

    fix: transfer the users storage from model to vm

--- lib/screens/home_page/view_model/home_view_model.dart
+++ lib/screens/home_page/view_model/home_view_model.dart
@@ -1,6 +1,54 @@
 import 'package:whatsapp/screens/home_page/model/user_model.dart';
 
-class UserViewModel {
+class HomeViewModel {
+  List<Users> dummy = [
+    Users(
+        userName: "Pujan",
+        messages: "Hey there! How's your day going?",
+        time: "2:00 AM",
+        imageURL: "assets/icon1.jpeg"),
+    Users(
+        userName: "Ram",
+        messages: "I just tried a new recipe.",
+        time: "3:00 AM",
+        imageURL: "assets/icon2.png"),
+    Users(
+        userName: "Shyam",
+        messages: "Do you believe in aliens?",
+        time: "6:00 AM",
+        imageURL: "assets/icon1.jpeg"),
+    Users(
+        userName: "Gita",
+        messages: "I can't wait for the weekend to relax.",
+        time: "2:03 PM",
+        imageURL: "assets/icon2.png"),
+    Users(
+        userName: "Sita",
+        messages: "Have you ever been on a trip?",
+        time: "3:20 AM",
+        imageURL: "assets/icon1.jpeg"),
+    Users(
+        userName: "Rakesh",
+        messages: "What's your favorite book of all time?",
+        time: "3:00 AM",
+        imageURL: "assets/icon2.png"),
+    Users(
+        userName: "Abishek",
+        messages: "Just finished a great workout.",
+        time: "6:00 AM",
+        imageURL: "assets/icon1.jpeg"),
+    Users(
+        userName: "Rita",
+        messages: "Thinking about starting a new hobby.",
+        time: "2:50 PM",
+        imageURL: "assets/icon2.png"),
+    Users(
+        userName: "Nita",
+        messages: "The weather today is so unpredictable.",
+        time: "2:03 PM",
+        imageURL: "assets/icon1.jpeg")
+  ];
+
   Map<String, String> userInfo(int index) {
     return {
       "userName": dummy[index].userName!,
