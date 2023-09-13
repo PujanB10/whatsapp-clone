ebc378e0c2457cf6dc1b14381fea0ee7e468c6d7
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-13 17:55:36 +0545

    fix: add comments on chat page

--- lib/screens/chat_page/chat_page.dart
+++ lib/screens/chat_page/chat_page.dart
@@ -5,11 +5,15 @@ import 'package:whatsapp/screens/chat_page/widgets/bottom_nav_bar_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:whatsapp/screens/chat_page/view_model/chat_view_model.dart';
 
-/// Private chat page of the app.
+/// A private chat page for viewing and sending messages.
 class ChatPage extends StatelessWidget {
-  /// Private chat page that opens after having clicked
-  /// a particular user from the home page. Takes [usrName],[imgUrl],[message]
-  /// as arguments.
+  /// Creates a [ChatPage] widget.
+  ///
+  /// [usrName], [imgUrl] and [message] must not be null.
+  ///
+  /// [usrName] is the username, [imgUrl] is the Url of image and [message] is the
+  /// last message of the user of whom the private chat page is to
+  /// be opened.
   const ChatPage(
       {required this.usrName,
       required this.imgUrl,
