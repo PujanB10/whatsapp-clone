51969f8eed32e3f3842570f3371b081d6ba4f98a
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-13 13:18:54 +0545

    fix: initialize change notifier in the chat page

--- lib/screens/chat_page/chat_page.dart
+++ lib/screens/chat_page/chat_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp/screens/chat_page/widgets/message_view_widget.dart';
 import 'package:whatsapp/screens/chat_page/widgets/app_bar_widget.dart';
 import 'package:whatsapp/screens/chat_page/widgets/bottom_nav_bar_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsapp/screens/chat_page/view_model/chat_view_model.dart';
 
 /// Private chat page of the app.
 class ChatPage extends StatelessWidget {
@@ -18,23 +20,25 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.blueGrey.shade100,
+    return ChangeNotifierProvider(
+        create: (context) => ChatViewModel(),
+        child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          backgroundColor: Colors.blueGrey.shade100,
 
-      /// Calls the custom AppBar
-      appBar: AppBarWidget(
-        username: usrName,
-        imageUrl: imgUrl,
-      ),
+          /// Calls the custom AppBar
+          appBar: AppBarWidget(
+            username: usrName,
+            imageUrl: imgUrl,
+          ),
 
-      /// Calls the chat message builder.
-      body: MessagesViewWidget(usrName: usrName),
+          /// Calls the chat message builder.
+          body: MessagesViewWidget(usrName: usrName),
 
-      /// Calls the Bottom Navigation Bar widget.
-      bottomNavigationBar: BottomNavBar(
-        usrName: usrName,
-      ),
-    );
+          /// Calls the Bottom Navigation Bar widget.
+          bottomNavigationBar: BottomNavBar(
+            usrName: usrName,
+          ),
+        ));
   }
 }
