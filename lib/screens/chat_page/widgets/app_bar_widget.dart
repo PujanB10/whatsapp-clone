7010843f7270e940f3aa2d564fc85967b3c35950
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-09-13 17:56:21 +0545

    fix: add comments on app bar widget

--- lib/screens/chat_page/widgets/app_bar_widget.dart
+++ lib/screens/chat_page/widgets/app_bar_widget.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-/// Custom AppBar widget for chatpages.
-
+/// A customizable appbar for displaying information and other functionalities.
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
-  /// Takes username, URL of the image of the user
-  /// and background color of the app bar and creates an AppBar with
-  /// video-call and audio-call icons integrated.
+  /// Creates a [AppBarWidget] widget.
+  ///
+  /// [username] and [imageUrl] must not be null.
+  ///
+  /// This appbar also consists of call, video-call and other options icons
+  /// integrated in it.
   const AppBarWidget({
     super.key,
     required this.username,
@@ -14,7 +16,13 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => const Size.fromHeight(55);
+
+  /// [username] is the username  of the
+  /// user whose information is to be displayed in the appbar.
   final String username;
+
+  /// [imageUrl] is the URL to the image of the
+  /// user whose information is to be displayed in the appbar.
   final String imageUrl;
 
   @override
@@ -30,7 +38,10 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
         ],
       ),
       actions: <Widget>[
-        IconButton(onPressed: () {}, icon: const Icon(Icons.video_call_sharp)),
+        /// Builds  call, video-call and options icons at the end of
+        /// the appbar.
+        IconButton(
+            onPressed: () {}, icon: const Icon(Icons.video_call_rounded)),
         IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
         IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
       ],
